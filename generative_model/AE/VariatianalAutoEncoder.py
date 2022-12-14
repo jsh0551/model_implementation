1e0d4fae04482f95907ea7be30b4e6af7ae1f941
jsh0551 <jsh941108@gmail.com>
2022-12-15 01:53:45 +0900

    VAE, CVAE

---
+++ generative_model/AE/VariatianalAutoEncoder.py
@@ -0,0 +1,47 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+
+class VAE(nn.Module):
+    def __init__(self, x_dim, h_dim1, h_dim2, z_dim):
+        super(VAE, self).__init__()
+        
+        self.fc1 = nn.Linear(x_dim, h_dim1)
+        self.fc2 = nn.Linear(h_dim1, h_dim2)
+        self.fc31 = nn.Linear(h_dim2, z_dim)
+        self.fc32 = nn.Linear(h_dim2, z_dim)
+
+        self.fc4 = nn.Linear(z_dim, h_dim2)
+        self.fc5 = nn.Linear(h_dim2, h_dim1)
+        self.fc6 = nn.Linear(h_dim1, x_dim)
+        
+    def encoder(self, x):
+        h = F.relu(self.fc1(x))
+        h = F.relu(self.fc2(h))
+        return self.fc31(h), self.fc32(h)
+    
+    def sampling(self, mu, log_var):
+        std = torch.exp(0.5*log_var)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu)
+        
+    def decoder(self, z):
+        h = F.relu(self.fc4(z))
+        h = F.relu(self.fc5(h))
+        return F.sigmoid(self.fc6(h)) 
+    
+    def forward(self, x):
+        mu, log_var = self.encoder(x.view(-1, 784))
+        z = self.sampling(mu, log_var)
+        return self.decoder(z), mu, log_var
+
+
+class VAE_Loss(nn.MSELoss):
+    def __init__(self, dim=784):
+        super(VAE_Loss,self).__init__()
+        self.dim = dim
+
+    def forward(self, recon_x, x, mu, log_var):
+        BCE = F.binary_cross_entropy(recon_x, x.view(-1, self.dim), reduction='sum')
+        KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+        return BCE + KLD
--- generative_model/AE/__init__.py
+++ generative_model/AE/__init__.py
@@ -1,1 +1,3 @@
 from .AutoEncoder import AutoEncoder
+from .VariatianalAutoEncoder import VAE,VAE_Loss
+from .ConditionalVAE import CVAE, CVAE_Loss
